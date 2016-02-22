3d3e86f5d2d60c6ea1c8b435a6c3fc4881805e9d
laaroussiBadr <badreddinelaaroussi@gmail.com>
2016-02-22 17:40:35 +0000

    Third Commit

--- templates/users.js
+++ templates/users.js
@@ -1,6 +1,6 @@
 var app = angular.module('myApp', []);
 
-app.controller('PostCtrl', function($scope, $http) {
+app.controller('MainCtrl', function($scope, $http) {
     $http.get("http://127.0.0.1:5000/api/post")
     .success(function(response) {
     	$scope.posts = response.objects;
@@ -9,24 +9,4 @@ app.controller('PostCtrl', function($scope, $http) {
     	{
     		console.log(response);
     	});
-  });
-
-
-app.controller('Post', function($scope, $http) {
-	$scope.text = "";
-	$scope.title = "";
-	$scope.date = new Date();
-
-	$scope.submit = function() {
-		var parameter = JSON.stringify({title:$scope.title, text:$scope.text, date:$scope.date, user_id:"1"});
-
-		$http.post("http://127.0.0.1:5000/api/post",parameter)
-		.success(function(response) {
-
-    	})
-		.error(function(response)
-		{
-			console.log(response);
-		});
-	}
   });
